ae7b6dc8dda783a692f390d9f3546de2f5fac5d4
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-14 19:21:13 +0100

    Set up Apollo, all todos query

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 import {authLogin, authLogout, authSignup} from "./auth";
 import { getTodos } from "./api";
 
+import ApolloClientBoost from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 function App() {
+
+  /**
+   * REST
+   */
+
   const signUp = e => {
     e.preventDefault();
-    authSignup({ email: 'john@doe.com', password: 'johndoe123' });
+    authSignup({ email: 'user@example.com', password: 'userexamplepwd' });
   };
 
   const logIn = e => {
     e.preventDefault();
-    authLogin({ email: 'john@doe.com', password: 'johndoe123' })
+    authLogin({ email: 'user@example.com', password: 'userexamplepwd' })
   };
 
   const logOut = e => {
@@ -25,20 +38,62 @@ function App() {
     getTodos();
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  /**
+   * Apollo
+   */
 
-        <button onClick={signUp}>Sign Up</button>
-        <button onClick={logIn}>Log In</button>
-        <button onClick={logOut}>Log Out</button>
-        <button onClick={fetchTodos}>Get todos</button>
-      </header>
-    </div>
+  const clientBoost = new ApolloClientBoost({
+    uri: 'http://localhost:3000/graphql',
+    fetchOptions: {
+      credentials: 'include', // does not include cookies
+    },
+    request: async operation => {
+      operation.setContext({
+        headers: {
+          'X-CSRF-TOKEN': localStorage.csrf
+        }
+      })
+    }
+  });
+
+  const client = new ApolloClient({
+    link: createHttpLink({
+      uri: 'http://localhost:3000/graphql', // only local URI (localhost:3001) -> ApolloClient from 'apollo-client'
+      credentials: 'include',
+      headers: {
+        'X-CSRF-TOKEN': localStorage.csrf
+      }
+    }),
+    cache: new InMemoryCache()
+  });
+
+  const FETCH_TODOS = gql`
+    { allTodos { title } }
+  `;
+
+  const fetchTodosGQL = () => {
+    client
+      .query({ query: FETCH_TODOS })
+      .then(result => console.log(result));
+  };
+
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <div>
+          <h2>REST</h2>
+          <button onClick={signUp}>Sign Up</button>
+          <button onClick={logIn}>Log In</button>
+          <button onClick={logOut}>Log Out</button>
+          <button onClick={fetchTodos}>Get todos</button>
+        </div>
+
+        <div>
+          <h2>GraphQL</h2>
+          <button onClick={fetchTodosGQL}>Get todos</button>
+        </div>
+      </div>
+    </ApolloProvider>
   );
 }
 
